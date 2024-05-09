cb23006900dfacf1e07cfa5b58e29ddf7d1055af
katsuyama-youhei <K022G0044@g.neec.ac.jp>
2024-05-09 11:23:34 +0900

    べた書きでステージをマップチップで表示

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var playerPosition = player.transform.position;
+        var position = transform.position;
+        position.x = playerPosition.x;
+        position.y = playerPosition.y+2;
+        position.z = playerPosition.z-8;
+        transform.position = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerPosition = player.transform.position;
+        var position = transform.position;
+        position.x = playerPosition.x;
+        position.y = playerPosition.y + 2;
+        position.z = playerPosition.z - 8;
+        transform.position = position;
+    }
+}
