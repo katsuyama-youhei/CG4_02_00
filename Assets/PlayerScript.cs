284d5e937793147681a4b8ccee779490142dfa1e
katsuyama-youhei <K022G0044@g.neec.ac.jp>
2024-05-22 14:39:03 +0900

    rayでプレイヤーの下にブロックがあるかの確認をとれるように

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -8,6 +8,9 @@ public class PlayerScript : MonoBehaviour
     public float moveSpeed = 1.0f;
     public float jumpSpeed = 1.0f;
 
+    // ray—p
+    float distance = 0.6f;
+    private bool isCollisionBlock = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +24,19 @@ public class PlayerScript : MonoBehaviour
         {
             Jump();
         }
+        // ƒŒƒC‚ð‰º•ûŒü‚É”ò‚Î‚·
+        Vector3 rayPosition = transform.position;
+        Ray ray = new Ray(rayPosition, Vector3.down);
+        //Debug.DrawRay(rayPosition, Vector3.down * distance, Color.red);
+        isCollisionBlock = Physics.Raycast(ray, distance);
+      /*  if (isCollisionBlock)
+        {
+            Debug.DrawRay(rayPosition, Vector3.down * distance, Color.red);
+        }
+        else
+        {
+            Debug.DrawRay(rayPosition, Vector3.down * distance, Color.yellow);
+        }*/
     }
 
     private void FixedUpdate()
